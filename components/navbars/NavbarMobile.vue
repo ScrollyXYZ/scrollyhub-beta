6d5504a977018c5f72b079280441419b7024993b
Scrolly The Map <156605610+ScrollyXYZ@users.noreply.github.com>
2024-06-17 12:00:54 +0000

    Navbar mobile white transparent

--- components/navbars/NavbarMobile.vue
+++ components/navbars/NavbarMobile.vue
@@ -5,18 +5,22 @@
         @click="toggleLeftSidebar"
         class="nav-item btn navbar-toggler nav-btn-left"
         type="button"
-        style="color: white; border-color: white;"
+        style="color: black; border-color: black"
       >
         <span
           v-if="!sidebarStore.showLeftSidebar"
           class="navbar-toggler-icon"
-          style="color: white;"
+          style="color: black"
         ></span>
-        <span v-if="sidebarStore.showLeftSidebar" class="bi bi-x-lg" style="color: white;">
+        <span
+          v-if="sidebarStore.showLeftSidebar"
+          class="bi bi-x-lg"
+          style="color: black"
+        >
         </span>
       </button>
 
-      <NuxtLink class="navbar-brand mx-auto" to="/" >
+      <NuxtLink class="navbar-brand mx-auto" to="/">
         <img src="/img/favicon.png" alt="Chat logo" height="40" />
         <span class="navbar-brand-text ms-2 align-middle">Scrolly Hub</span>
       </NuxtLink>
