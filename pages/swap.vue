9831cde5dc8e72be2a387a04ad3e41e852c01220
Scrolly The Map <156605610+ScrollyXYZ@users.noreply.github.com>
2024-06-20 08:44:31 +0000

    Add Prevents message on Swap

--- pages/swap.vue
+++ pages/swap.vue
@@ -18,7 +18,20 @@
         </p>
 
         <h3 class="mt-3">Token Swap</h3>
+        <h4>
+          Powered by
+          <img
+            src="/img/Zprotocol.png"
+            alt="ZProtocol Logo"
+            width="50"
+            height="50"
+          />
+        </h4>
 
+        In Partnership with Z Protocol & IggySocial, you can directly swap on
+        the Hub. The swap will occur with Zprotocol pools. As a reminder, you
+        can also dive in by adding liquidity and then head over to the "Earn"
+        section to start earning passive income.
         <div class="d-flex justify-content-center mt-5">
           <div class="col-12 col-lg-8">
             <SimpleSwap
