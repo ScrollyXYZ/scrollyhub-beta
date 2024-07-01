5a268e82b019e51eb60e18fbd2fc63dacf360d69
Scrolly The Map <156605610+ScrollyXYZ@users.noreply.github.com>
2024-07-01 08:48:14 +0000

    Remove vue

--- components/badge/BadgeSticker.vue
+++ components/badge/BadgeSticker.vue
@@ -3,7 +3,9 @@
     <div class="tooltip-container" v-if="showPopup">
       <button class="close-button" @click="closeContainer">×</button>
       <img src="/skelly/tooltip.svg" alt="Tooltip" class="tooltip-img" />
-      <span class="rainbow-text">You are eligible to mint the Scrolly Badge</span>
+      <span class="rainbow-text"
+        >You are eligible to mint the Scrolly Badge</span
+      >
       <NuxtLink to="/badge">
         <button class="mint-button">Mint Badge</button>
       </NuxtLink>
@@ -13,22 +15,21 @@
     </NuxtLink>
   </div>
 </template>
-
 <script>
 export default {
   name: "BadgeSticker",
   data() {
     return {
       showPopup: true,
-      showContainer: true
-    }
+      showContainer: true,
+    };
   },
   methods: {
     closeContainer() {
       this.showContainer = false;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style>
@@ -38,12 +39,11 @@ export default {
   width: 300px;
   bottom: 10px;
   right: 20px;
-  display: flex; 
-  flex-direction: column; 
-  justify-content: center; 
-  align-items: center; 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
-
 .tooltip-container {
   display: flex;
   justify-content: center;
@@ -51,26 +51,32 @@ export default {
   position: relative;
   flex-direction: column;
 }
-
 .rainbow-text {
   position: absolute;
   left: 50%;
   transform: translate(-50%, -50%);
   text-align: center;
   z-index: 1;
-  background: linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet);
+  background: linear-gradient(
+    to right,
+    red,
+    orange,
+    yellow,
+    green,
+    blue,
+    indigo,
+    violet
+  );
   background-size: 200% 200%;
   -webkit-background-clip: text;
   background-clip: text;
   color: transparent;
   animation: rainbow 5s ease-in-out infinite;
-  width: 100%; 
-  height: auto; 
-  box-sizing: border-box; 
-  padding: 5px 10px; 
+  width: 100%;
+  height: auto;
+  box-sizing: border-box;
+  padding: 5px 10px;
 }
-
-
 @keyframes rainbow {
   0% {
     background-position: 0 50%;
@@ -82,7 +88,6 @@ export default {
     background-position: 0 50%;
   }
 }
-
 .mint-button {
   position: absolute;
   top: 55%;
@@ -92,11 +97,10 @@ export default {
   background-color: var(--bs-purple);
   color: white;
   cursor: pointer;
-  width: 90%; 
-  height: auto; 
-  box-sizing: border-box; 
+  width: 90%;
+  height: auto;
+  box-sizing: border-box;
 }
-
 .close-button {
   position: absolute;
   color: black;
@@ -107,17 +111,13 @@ export default {
   font-size: 20px;
   cursor: pointer;
 }
-
 /* Mobile only */
 @media only screen and (max-width: 767px) {
   .badge-sticker-container {
-    padding: 5px;
-    height: auto;
-    width: 100px;
-    left: 50%;
-    transform: translateX(-50%);
-    right: auto;
+    @@ -95,5 +118,6 @@ export default {
+      transform: translateX(-50%);
+      right: auto;
+    }
   }
-
 }
 </style>
