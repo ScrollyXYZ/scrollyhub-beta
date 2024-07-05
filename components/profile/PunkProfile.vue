b100754a5af47e92fbf48f0fbb81a4d37ce6752d
Scrolly The Map <156605610+ScrollyXYZ@users.noreply.github.com>
2024-07-05 08:38:40 +0000

    Add ChangeUsernameModal

--- components/profile/PunkProfile.vue
+++ components/profile/PunkProfile.vue
@@ -337,6 +337,7 @@
       </div>
     </div>
   </div>
+  <ChangeUsernameModal :address="uAddress" />
 </template>
 
 <script>
@@ -352,6 +353,7 @@ import ChatFeed from "../chat/ChatFeed.vue";
 import { getActivityPoints } from "~/utils/balanceUtils";
 import { getDomainName, getDomainHolder } from "~/utils/domainUtils";
 import { fetchUsername, storeUsername } from "~/utils/storageUtils";
+import ChangeUsernameModal from "~/components/names/ChangeUsernameModal.vue";
 import { getTextWithoutBlankCharacters } from "~/utils/textUtils";
 
 export default {
@@ -387,6 +389,7 @@ export default {
     ProfileImage,
     UserCollections,
     UserMintedPosts,
+    ChangeUsernameModal,
   },
 
   mounted() {
@@ -730,7 +733,6 @@ export default {
 </script>
 
 <style scoped>
-
 .profile-image-column {
   text-align: center;
 }
@@ -867,7 +869,6 @@ export default {
 .profile-back-button {
   display: none;
 }
-
 @media (max-width: 768px) {
   .profile-back-button {
     display: block;
