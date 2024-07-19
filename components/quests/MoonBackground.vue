7713df95f98922bcadcdd4622e8413eac8ab1528
Scrolly The Map <156605610+ScrollyXYZ@users.noreply.github.com>
2024-07-19 12:48:53 +0200

    Update MoonBackground.vue

--- components/quests/MoonBackground.vue
+++ components/quests/MoonBackground.vue
@@ -6,18 +6,6 @@
       alt="Background"
       class="background-image"
     />
-    <video
-      ref="backgroundVideo"
-      autoplay
-      muted
-      loop
-      playsinline
-      class="background-video"
-      @canplaythrough="onVideoLoad"
-    >
-      <source :src="fullVideoUrl" type="video/mp4" />
-      Your browser doesn't support video format.
-    </video>
     <div class="overlay" v-if="isDarkMode"></div>
   </div>
 </template>
@@ -34,25 +22,16 @@ export default {
   data() {
     return {
       videoLoaded: false,
-      baseVideoUrl: useRuntimeConfig().public.r2BaseUrl,
-      videoPath: "/backgroundquest.mp4",
     };
-  },
-  computed: {
-    fullVideoUrl() {
-      return `${this.baseVideoUrl}${this.videoPath}`;
-    },
   },
   methods: {
     onVideoLoad() {
       this.videoLoaded = true;
-      if (this.$refs.backgroundImage) {
-        this.$refs.backgroundImage.style.display = "none";
-      }
+      this.$refs.backgroundImage.style.display = "none";
     },
   },
   mounted() {
-    this.$refs.backgroundVideo.playbackRate = 1;
+    // Removed the video playbackRate setting as there is no video anymore
   },
 };
 </script>
@@ -70,6 +49,7 @@ export default {
   overflow: hidden;
   z-index: -1;
 }
+
 .background-image {
   position: absolute;
   top: 0;
@@ -80,16 +60,7 @@ export default {
   z-index: -2;
   display: block;
 }
-.background-video {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  transform: translate(-50%, -50%);
-  z-index: -3;
-}
+
 .overlay {
   position: absolute;
   top: 0;
